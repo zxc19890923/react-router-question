04d49b197e30661facb68c9ab39ef0189052e10a
zhangxuchao <2212362272@qq.com>
2016-12-01 10:29:31 +0800

    Update react-router问题汇总.jsx

--- react-router问题汇总.jsx
+++ react-router问题汇总.jsx
@@ -6,3 +6,11 @@ import {Router, Route, browserHistory} from "react-router";
 <Router history={browserHistory}>
 
 </Router>
+
+2. 路由中，配置完基本路由以后，首页加载子组件的内容，显示界面， {this.props.children}，
+  但是在初始界面，还没有单机路由的时候，{this.props.children}为空undefined,所有初始化界面为空，为了默认界面，我们使用IndexRouter
+
+import {IndexRouter} from "react-router";
+
+<IndexRouter component={Init} />
+这样就设定了默认首界面/显示的内容
